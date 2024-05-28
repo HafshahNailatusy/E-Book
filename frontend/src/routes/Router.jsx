8179f0676d012b72fedc893948a86cc43bdce93e
zmrashalaila <shalailaaaz41@gmail.com>
2024-05-28 16:34:39 +0700

    fix: router

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -4,19 +4,19 @@ import ProtectRoute from "./ProtectedRoute";
 import AdminRoute from "./AdminRoute";
 import LoadingSpinner from "../components/Loading";
 
-const Login = lazy(() => import("../pages/Login/Login"));
-const LoginAdmin = lazy(() => import("../pages/Login/LoginAdmin"));
-const Register = lazy(() => import("../pages/Login/Register"));
-const DashboardUser = lazy(() => import("../pages/Dashboard"));
-const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
-const ForbiddenPage = lazy(() => import("../pages/ForbiddenPage"));
-const Aplikasi = lazy(() => import("../pages/Books"));
-const User = lazy(() => import("../pages/User"));
-const Transaksi = lazy(() => import("../pages/Transaction"));
-const Profile = lazy(() => import("../pages/Profile"));
-const History = lazy(() => import("../pages/History"));
-const Order = lazy(() => import("../pages/Order"));
-const ErrorPage = lazy(() => import("../pages/ErrorPage"));
+const Login = lazy(() => import("../Pages/Login/Login"));
+const LoginAdmin = lazy(() => import("../Pages/Login/LoginAdmin"));
+const Register = lazy(() => import("../Pages/Login/Register"));
+const DashboardUser = lazy(() => import("../Pages/Dashboard"));
+const NotFoundPage = lazy(() => import("../Pages/NotFoundPage"));
+const ForbiddenPage = lazy(() => import("../Pages/ForbiddenPage"));
+const Aplikasi = lazy(() => import("../Pages/Books"));
+const User = lazy(() => import("../Pages/User"));
+const Transaksi = lazy(() => import("../Pages/Transaction"));
+const Profile = lazy(() => import("../Pages/Profile"));
+const History = lazy(() => import("../Pages/History"));
+const Order = lazy(() => import("../Pages/Order"));
+const ErrorPage = lazy(() => import("../Pages/ErrorPage"));
 
 const Router = () => {
   return (
