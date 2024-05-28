2b4a896c05e18c8152f321d9a2195924fef5d43b
tasyaswantara <tasyaswantara@gmail.com>
2024-05-28 21:34:17 +0700

    fix: navbar admin

--- frontend/src/components/Navbar/NavbarAdmin.jsx
+++ frontend/src/components/Navbar/NavbarAdmin.jsx
@@ -1,11 +1,12 @@
 const NavbarAdmin = () => {
   return (
-    <header className="w-full md:w-[32rem] h-20 fixed rounded-t-3xl md:rounded-t-none md:rounded-br-3xl bottom-0 md:top-0 flex bg-primary-dark justify-center md:justify-start items-center px-4 md:px-8 z-50 shadow-lg">
+    
+    <header className="  w-full flex justify-center md:justify-center items-center px-4 md:px-8 z-50 shadow-lg">
       <ul className="flex space-x-12 items-center">
         <li className="my-6">
           <a
             href="/"
-            className="text-lg text-white hover:text-slate-300 font-semibold duration-500"
+            className="text-lg text-black hover:text-slate-300 font-semibold duration-500"
           >
             Home
           </a>
@@ -13,7 +14,7 @@ const NavbarAdmin = () => {
         <li className="my-6">
           <a
             href="/admin/buku"
-            className="text-lg text-white hover:text-slate-300 font-semibold duration-500"
+            className="text-lg text-black hover:text-slate-300 font-semibold duration-500"
           >
             Buku
           </a>
@@ -21,7 +22,7 @@ const NavbarAdmin = () => {
         <li className="my-6">
           <a
             href="/admin/user"
-            className="text-lg text-white hover:text-slate-300 font-semibold duration-500"
+            className="text-lg text-black hover:text-slate-300 font-semibold duration-500"
           >
             User
           </a>
@@ -29,7 +30,7 @@ const NavbarAdmin = () => {
         <li className="my-6">
           <a
             href="/admin/transaksi"
-            className="text-lg text-white hover:text-slate-300 font-semibold duration-500"
+            className="text-lg text-black hover:text-slate-300 font-semibold duration-500"
           >
             Transaksi
           </a>
